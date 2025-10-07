6b965e7ca5766062525617d711e0084abc1767b5
Kim Jaehwan <carotinoid1807@gmail.com>
2025-10-08 02:01:59 +0900

    feat: implement assign 6: streams

--- streams/src/main/scala/streams/StringParserTerrain.scala
+++ streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,7 +52,13 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = {
+    pos => {
+      if (pos.x < 0 || pos.x >= levelVector.length) false
+      else if(pos.y < 0 || pos.y >= levelVector(pos.x).length) false
+      else levelVector(pos.x)(pos.y) != '-'
+    }
+  }
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +68,11 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    val x = levelVector.indexWhere(_.indexOf(c) != -1)
+    val y = levelVector(x).indexOf(c)
+    Pos(x, y)
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -40,6 +40,7 @@ class BloxorzSuite extends FunSuite {
     val optsolution = List(Right, Right, Down, Right, Right, Right, Down)
   }
 
+
   test("terrain function level 1") {
     new Level1 {
       assert(terrain(Pos(0,0)), "0,0")
@@ -50,6 +51,55 @@ class BloxorzSuite extends FunSuite {
   test("findChar level 1") {
     new Level1 {
       assert(startPos == Pos(1,1))
+    }
+  }
+
+  test("finding neighbors") {
+    new Level1 {
+      val a = neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up)).toSet
+      val b = Set(
+                    (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+                    (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+                 )
+      assert(a === b, "Finding neighbors failed")
+    }
+  }
+
+  test("avoiding circles") {
+    new Level1 {
+      val a = newNeighborsOnly(
+                Set(
+                  (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+                  (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+                ).toStream,
+
+                Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+              )
+      val b = Set(
+                (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+              ).toStream
+      assert(a === b, "Avoiding circles failed")
+    }
+  }
+
+  trait Level0 extends SolutionChecker {
+    val level = 
+      """ST
+        |oo
+        |oo""".stripMargin
+
+    val optsolution = List(Down, Right, Up)
+  }
+
+  test("optimal solution for level 0") {
+    new Level0 {
+      assert(solve(solution) == Block(goal, goal))
+    }
+  }
+
+  test("optimal solution length for level 0") {
+    new Level0 {
+      assert(solution.length == optsolution.length)
     }
   }
 
@@ -64,4 +114,88 @@ class BloxorzSuite extends FunSuite {
       assert(solution.length == optsolution.length)
     }
   }
+
+
+  trait Level3 extends SolutionChecker {
+    val level = 
+    """------ooooooo--
+      |oooo--ooo--oo--
+      |ooooooooo--oooo
+      |oSoo-------ooTo
+      |oooo-------oooo
+      |------------ooo""".stripMargin
+
+    val calculatedOptSolution = List(Right, Up, Right, Right, Right, Up, Left, Down, Right, Up, Up, Right, Right, Right, Down, Down, Down, Right, Up)
+  }
+
+  test("optimal solution for level 3") {
+    new Level3 {
+      assert(solve(solution) == Block(goal, goal))
+    }
+  }
+
+  test("manual check optimal solution for level 3") {
+    new Level3 {
+      assert(solution == calculatedOptSolution)
+    }
+  }
+
+  trait Level6 extends SolutionChecker {
+    val level =
+    """-----oooooo----
+      |-----o--ooo----
+      |-----o--ooooo--
+      |Sooooo-----oooo
+      |----ooo----ooTo
+      |----ooo-----ooo
+      |------o--oo----
+      |------ooooo----
+      |------ooooo----
+      |-------ooo-----""".stripMargin
+
+    val calculatedOptSolution = List(Right, Right, Right, Down, Right, Down, Down, Right, Down, Down, Right, Up, Left, Left, Left, Up, Up, Left, Up, Up, Up, Right, Right, Right, Down, Down, Left, Up, Right, Right, Down, Right, Down, Down, Right)
+  }
+
+  test("optimal solution for level 6") {
+    new Level6 {
+      assert(solve(solution) == Block(goal, goal))
+    }
+  }
+
+  test("manual check optimal solution for level 6") {
+    new Level6 {
+      assert(solution == calculatedOptSolution)
+    }
+  }
+
+  trait Level7 extends SolutionChecker {
+    val level = 
+    """--------oooo---
+      |--------oooo---
+      |ooo-----o--oooo
+      |oSooooooo---oTo
+      |ooo----ooo--ooo
+      |ooo----ooo--ooo
+      |-ooo---o-------
+      |--oooooo-------""".stripMargin
+
+    // To turn on the switch and return to startPos, 
+    // Down, Left, Up, Right, Right, Right, Right, Right, Down, Right, 
+    // Left, Up, Left, Left, Left, Left, Left, Down, Right, Up
+
+    val calculatedOptSolution = List(Down, Down, Right, Down, Right, Right, Right, Up, Up, Right, Down, Left, Up, Right, Up, Up, Right, Right, Right, Down, Right, Down, Right, Down, Left, Up)
+  }
+
+  test("optimal solution for level 7") {
+    new Level7 {
+      assert(solve(solution) == Block(goal, goal))
+    }
+  }
+
+  test("manual check optimal solution for level 7") {
+    new Level7 {
+      assert(solution == calculatedOptSolution)
+    }
+  }
+
 }
